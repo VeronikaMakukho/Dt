c5323290fc7a4d7cb4c8fe6ac166b7d50b1adab2
VeronikaMakukho <40073163+VeronikaMakukho@users.noreply.github.com>
2018-06-08 22:19:40 +0200

    Added new solution project

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxMass = 0;
+            int[] mass = { 1, 1, 1, 1, 2, 3, 2 };
+            for (int i = 0; i < mass.Length; i++)
+            {
+                int summLeft = 0;
+                int summRigh = 0;
+                //Summ of left sight 
+                for (int j = 0; j < i; j++)
+                {
+                    summLeft += mass[j];
+                }
+                //Summ of right sight 
+                for (int j = i + 1; j < mass.Length; j++)
+                {
+                    summRigh += mass[j];
+                }
+
+                if (summLeft == summRigh)
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+                else if (summLeft != summRigh && i == mass.Length - 1)
+                {
+                    Console.WriteLine("None index match");
+                    Console.ReadLine();
+                    return;
+                }
+            }
+            Console.ReadLine();
+        }
---
+++ ConsoleApp1/Program2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxMass = 0;
+            int[] mass = { 5, 1, 4, 10, 15, 14, 2, 1 };
+            Max(mass, ref maxMass, 0);
+            Console.WriteLine(maxMass);
+            Console.ReadLine();
+        }
+        static void Max(int[] _mass, ref int max, int massIndex)
+        {
+            if (massIndex == _mass.Length) { return; }
+            if (max < _mass[massIndex])
+            {
+                max = _mass[massIndex];
+                //massIndex += 1; 
+                //Max(_mass, ref max, massIndex); 
+            }
+            massIndex = (massIndex < _mass.Length) ? massIndex + 1 : massIndex;
+            Max(_mass, ref max, massIndex);
+        }
